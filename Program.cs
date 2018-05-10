52dd0c1f29eb1311df91654c95a8543260a81b65
James Young <imjamesyoung@gmail.com>
2018-05-09 21:42:40 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//this reads other codes that microsoft used to run this program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*everything
+ * related */
+namespace HelloWorld
+{
+    class Program //"blueprint"
+    {
+        static void Main(string[] args) //block of code that performs a specific task
+        {
+            //Displays sentence
+            Console.WriteLine("Hello World");
+            Console.ReadKey(); //pauses
+        }
+    }
+}
